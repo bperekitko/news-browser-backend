291313c39f9d3fae819169f0625f132bed65e81d
Bartlomiej Perekitko <b.perekitko@gmail.com>
2019-07-11 14:09:08 +0200

    Initial commit

---
+++ src/main/java/news/browser/NewsBrowserApplication.java
@@ -0,0 +1,13 @@
+package news.browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBrowserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsBrowserApplication.class, args);
+    }
+
+}
